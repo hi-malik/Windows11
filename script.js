1d909705d1923de68a2b34b04d76b3319891b4d1
Himanshu Malik <jsm.28415@gmail.com>
2021-07-01 08:04:40 +0530

    taskbar not working

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let taskbar = document.getElementsByClassName("taskbar")[0]
 let startmenu = document.getElementsByClassName("startmenu")[0]
 
 taskbar.addEventListener("click", () =>{
+    console.log("clicked");
     if(startmenu.style.bottom == "50px"){
         startmenu.style.bottom == "-650px"
     }
